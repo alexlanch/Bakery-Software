1f4f4bf3b34f101f100dadb41cf90e4099489941
Alex Lancheros <alex.pro.510@gmail.com>
2024-02-13 15:34:14 -0500

    Interfaz grafica MafinsPatry sin imagenes

---
+++ src/main/java/com/mycompany/mafinpastry2/MafinPastry2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mafinpastry2;
+
+/**
+ *
+ * @author ProBook 440 G7
+ */
+public class MafinPastry2 {
+
+    public static void main(String[] args) {
+        
+        Main gui = new Main();
+        gui.setVisible(true);
+        gui.setLocationRelativeTo(null);
+        //gui.setResizable(true);
+        /*gui.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        gui.setResizable(true);
+        gui.setTitle("ChatBot By Lancheros");*/
+    }
+}
